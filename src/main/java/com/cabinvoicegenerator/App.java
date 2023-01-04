9df83245ffebeb5fd3c11706c2ae8bd8084b8d71
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2023-01-04 16:19:49 +0530

    Add: Initial Commit

---
+++ src/main/java/com/cabinvoicegenerator/App.java
@@ -0,0 +1,13 @@
+package com.cabinvoicegenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to cab invoice generator!" );
+    }
+}
