6cf3d7f92525f6d0742a8c1253f88c8e972047c9
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2023-01-04 23:08:30 +0530

    Add: Codes to get Invoice as per Rides: Premium or Normal

--- src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -7,55 +7,69 @@ import org.junit.Test;
 
 public class InvoiceGeneratorTest {
 	InvoiceGenerator invoiceGenerator;
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance=2.0;
+        int time=5;
+        double fare = invoiceGenerator.calculateFare(distance, time );
+        Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance=0.1;
+        int time=1;
+        double fare = invoiceGenerator.calculateFare(distance, time );
+        Assert.assertEquals(5,fare,0.0);
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturnInvoiceSummary(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+        InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedSummary,invoiceSummary);
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturnInvoice(){
+        HashMap<Integer,Ride[]> rideRepo = new HashMap<>();
+        Ride[] rides1 ={new Ride(2.0,5),new Ride(0.1,1)};
+        Ride[] rides2 ={new Ride(10.0,2),new Ride(5.0,1)};
+        Ride[] rides3 ={new Ride(7.0,3),new Ride(5.0,1)};
+        Ride[] rides4 ={new Ride(4.0,3),new Ride(5.0,4)};
+        rideRepo.put(1,rides1);
+        rideRepo.put(2,rides2);
+        rideRepo.put(3,rides3);
+        rideRepo.put(4,rides4);
+        int userID = 1;
+        RideRepository rideRepository = new RideRepository(rideRepo,userID);
+        InvoiceSummary invoiceSummary = rideRepository.calculateFare();
+        InvoiceSummary expectedSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
 
-	@Before
-	public void setUp() throws Exception {
-		invoiceGenerator = new InvoiceGenerator();
-	}
+    }
+    @Test
+    public void givenMultipleRides_whenGivenWithCategories_shouldReturnInvoiceAccordingToType() {
+        Ride[] rides = {new Ride(2.0 , 5) , new Ride(5.0 , 8)};
 
-	@Test
-	public void givenDistanceAndTime_shouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-		double distance = 2.0;
-		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time);
-		Assert.assertEquals(25, fare, 0.0);
-	}
+        String category = "Premium";
 
-	@Test
-	public void givenDistanceAndTime_shouldReturnMinFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-		double distance = 0.1;
-		int time = 1;
-		double fare = invoiceGenerator.calculateFare(distance, time);
-		Assert.assertEquals(5, fare, 0.0);
-	}
+        if(category == "Premium") {
+            PremiumRides premiumInvoice = new PremiumRides();
 
-	@Test
-	public void givenDistanceAndTime_shouldReturnInvoiceSummary() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
-		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
-		Assert.assertEquals(expectedSummary, invoiceSummary);
-	}
-
-	@Test
-	public void givenDistanceAndTime_shouldReturnInvoice() {
-		HashMap<Integer, Ride[]> rideRepo = new HashMap<>();
-		Ride[] rides1 = { new Ride(2.0, 5), new Ride(0.1, 1) };
-		Ride[] rides2 = { new Ride(10.0, 2), new Ride(5.0, 1) };
-		Ride[] rides3 = { new Ride(7.0, 3), new Ride(5.0, 1) };
-		Ride[] rides4 = { new Ride(4.0, 3), new Ride(5.0, 4) };
-		rideRepo.put(1, rides1);
-		rideRepo.put(2, rides2);
-		rideRepo.put(3, rides3);
-		rideRepo.put(4, rides4);
-		int userID = 1;
-		RideRepository rideRepository = new RideRepository(rideRepo, userID);
-		InvoiceSummary invoiceSummary = rideRepository.calculateFare();
-		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
-		Assert.assertEquals(expectedSummary, invoiceSummary);
-
-	}
+            InvoiceSummary premiumInvoiceSummary = premiumInvoice.calculateFare(rides);
+            InvoiceSummary expectedPremiumInvoiceSummary = new InvoiceSummary(2, 131.0);
+            Assert.assertEquals(expectedPremiumInvoiceSummary, premiumInvoiceSummary);
+        }
+        else {
+            InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+            Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+        }
+    }
 }
